63877ca17899792e5f73eff264240110287da97f
aumza0746 <aumza0746@gmail.com>
2024-11-29 04:32:03 +0000

    first commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: const Text(
+          'Breakfast',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
